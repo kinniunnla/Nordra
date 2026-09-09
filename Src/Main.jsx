b38a8ccd6825d513d1e37ee5fa1ad2ec56ffdd6e
kinniunnla <adekaluifediamond@gmail.com>
2026-09-10 01:20:47 +0400

    Add Main component to render NordraOS

---
+++ Src/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NordraOS from "./NordraOS.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NordraOS />
+  </React.StrictMode>
+);
